4775f4edf42133ce2b52ab314a9300e6de75a5cd
maysa.alves@webjump.com.br <maysa bilac>
2022-01-17 00:56:02 -0300

    Curso de LESS finalizado!

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        files: {
+          "css/estilos.css": "css/estilos.less" // Caminho dos arquivos
+        }
+      }
+    },
+    
+      less: {
+        development: {
+          files: {
+            "css/layout/media-queries.css": "css/layout/media-queries.less"
+          }
+        }
+      },
+
+    watch: {
+      styles: {
+        files: ['**/*.less'], // Quais arquivos o grunt ficará de olho
+        tasks: ['less']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
